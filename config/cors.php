172c191614950d5b443ed60e7b64789347f572cc
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2022-11-04 12:44:00 +0800

    CORS

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000'), 'http://camp-api.university.tw', 'https://camp-api.university.tw'],
 
     'allowed_origins_patterns' => [],
 
