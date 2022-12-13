fc7866477ac09505e1695b456864f2e09dbdde88
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2022-12-13 16:08:28 +0800

    seed new data

--- database/seeders/CampSeeder.php
+++ database/seeders/CampSeeder.php
@@ -8,13 +8,15 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Carbon;
 
-class CampSeeder extends Seeder {
+class CampSeeder extends Seeder
+{
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run() {
+    public function run()
+    {
         foreach (array_reverse(json_decode(file_get_contents(resource_path('data-1102.json')))) as $item) {
             $sys = User::find(1);
 
@@ -47,7 +49,7 @@ class CampSeeder extends Seeder {
                 'url' => $item->url,
                 'recommend' => true,
                 'priority' => 101,
-                'status' => 1
+                'status' => 0
             ]);
             $camp->offers()->create([
                 'name' => '一般報名',
@@ -55,33 +57,39 @@ class CampSeeder extends Seeder {
                 'price' => $item->price,
             ]);
         }
-        foreach (array_reverse(json_decode(file_get_contents(resource_path('data-1120.json')))) as $item) {
-            $sys = User::find(1);
 
-            $camp = $sys->camps()->create([
-                'name' => $item->name,
-                'school' => $item->school,
-                'department' => $item->department,
-                'start' => $item->start,
-                'end' => $item->end ?? $item->start,
-                'apply_end' => $item->apply_end ?? Carbon::now()->addYear(),
-                'url' => $item->url,
-                'status' => 1
-            ]);
-            foreach ($item->offers as $offer) {
-                $camp->offers()->create([
-                    'name' => $offer->name,
-                    'description' => $offer->name,
-                    'price' => $offer->price,
+        foreach (['data-1120.json',
+                     'data-university.tw-1213.json'] as $path) {
+            foreach (array_reverse(json_decode(file_get_contents(resource_path($path)))) as $item) {
+                $sys = User::find(1);
+
+                $camp = $sys->camps()->create([
+                    'name' => $item->name,
+                    'school' => $item->school,
+                    'department' => $item->department,
+                    'start' => $item->start,
+                    'end' => $item->end ?? $item->start,
+                    'apply_end' => $item->apply_end ?? Carbon::now()->addYear(),
+                    'url' => $item->url,
+                    'status' => 1,
+                    'recommend' => $item->recommend ?? false,
+                    'priority' => $item->priority ?? 100,
                 ]);
+                foreach ($item->offers as $offer) {
+                    $camp->offers()->create([
+                        'name' => $offer->name,
+                        'description' => $offer->name,
+                        'price' => $offer->price,
+                    ]);
+                }
+                foreach ($item->tags as $tag) {
+                    $camp->tags = [
+                        ...($camp->tags ?? []),
+                        $tag
+                    ];
+                }
+                $camp->save();
             }
-            foreach ($item->tags as $tag) {
-                $camp->tags = [
-                    ...($camp->tags ?? []),
-                    $tag
-                ];
-            }
-            $camp->save();
         }
     }
 }
